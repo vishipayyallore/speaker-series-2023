b6a5a4fb9bd77ec00c4e85ed42282f74e4c3423f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-20 19:05:13 +0530

    Telemetry

---
+++ csharp-demos/DataReaderPoc/DataReaderPoc.Lib/Telemetery.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DataReaderPoc.Lib
+{
+    public class Telemetery : ITelemetery
+    {
+        public async Task<DbDataReader> GetMoviesList(SqlCommand sqlCommand)
+        {
+            return await sqlCommand.ExecuteReaderAsync();
+        }
+    }
+}
